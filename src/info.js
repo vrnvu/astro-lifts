6940d47b50b8e480f141e5dcd09402dd91473026
Arnau Diaz <diazarnau0@gmail.com>
2024-04-27 19:25:24 +0200

    init

--- src/info.js
+++ src/info.js
@@ -1,122 +1,31 @@
 export const i = {
-    firstName: "Jensen",
-    secondName: "Lakes",
-    occupation: "Digital Director",
-    description: "Hi, I'm Jensen. I have over 10 years of experience as a digital director with a multidisciplinary background. My main goal is to create meaningful products that cause positive disruption.",
-    status: "⚡︎ Open for remote/async offers",
-    email: "📧 jensen@jensenlakes.com",
-    photo: "../src/assets/pic.png",
+    description: "Workout Operator Black template",
     featuredWork: [
         {
-            title: "Company X",
-            dates: "2010 — 2015"
+            title: "Bench press",
+            dates: "3x5 - 70kg"
         },
         {
-            title: "Company Y",
-            dates: "2015 — 2020"
+            title: "Back squat",
+            dates: "3x5 - 90kg"
         },
         {
-            title: "Company Z",
-            dates: "2020 — now"
+            title: "Weighted pull ups",
+            dates: "3x5 - 20kg"
         },
     ],
     recentProjects: [
         {
-            title: "Project X",
-            url: "0xshapeshifter.dev"
+            title: "Run",
+            dates: "40 min"
         },
         {
-            title: "Project y",
-            url: "0xshapeshifter.dev"
+            title: "Run",
+            dates: "50 min"
         },
         {
-            title: "Project Z",
-            url: "0xshapeshifter.dev"
-        },
+            title: "Run",
+            dates: "60 min"
+        }
     ],
-    recognition: [
-        {
-            title: "Award X",
-            date: "2010"
-        },
-        {
-            title: "Award Y",
-            date: "2015"
-        },
-        {
-            title: "Award Z",
-            date: "2020"
-        },
-    ],
-    socialMedia: [
-        {
-            title: "X",
-            url: "https://google.com",
-            alt: "X logo",
-            images: {
-                dark: "../src/assets/social/dark/x.svg",
-                light: "../src/assets/social/light/x.svg",
-            }
-        },
-        {
-            title: "Mastodon",
-            url: "https://google.com",
-            alt: "X logo",
-            images: {
-                dark: "../src/assets/social/dark/mt.svg",
-                light: "../src/assets/social/light/mt.svg",
-            }
-        },
-        {
-            title: "Telegram",
-            url: "https://google.com",
-            alt: "X logo",
-            images: {
-                dark: "../src/assets/social/dark/tg.svg",
-                light: "../src/assets/social/light/tg.svg",
-            }
-        },
-        {
-            title: "Facebook",
-            url: "https://google.com",
-            alt: "X logo",
-            images: {
-                dark: "../src/assets/social/dark/fb.svg",
-                light: "../src/assets/social/light/fb.svg",
-            }
-        },
-        {
-            title: "Instagram",
-            url: "https://google.com",
-            alt: "X logo",
-            images: {
-                dark: "../src/assets/social/dark/ig.svg",
-                light: "../src/assets/social/light/ig.svg",
-            }
-        },
-        {
-            title: "LinkedIn",
-            url: "https://google.com",
-            alt: "X logo",
-            images: {
-                dark: "../src/assets/social/dark/lin.svg",
-                light: "../src/assets/social/light/lin.svg",
-            }
-        },
-        {
-            title: "Youtube",
-            url: "https://google.com",
-            alt: "X logo",
-            images: {
-                dark: "../src/assets/social/dark/yt.svg",
-                light: "../src/assets/social/light/yt.svg",
-            }
-        },
-    ],
-    // Thank you for leaving this part and supporting my work
-    support: {
-        sign: "＊",
-        title: "by 0xhapeshifter",
-        url: "https://0xshapeshifter.dev/productize"
-    }
 }
